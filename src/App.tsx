bcaabd39401aeb30c644bb53670eb7952bc9a9e7
JH <silkcarp97@gmail.com>
2022-02-27 00:45:32 +0900

    bulma practice 2/27

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "bulma/css/bulma.min.css";
 import Navbar from "component/navbar/Navbar";
+import Menu from "component/menu/Menu";
+import Main from "component/main/Main";
+
+import "./app.css";
+import Footer from "component/footer/Footer";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <div className="middle">
+        <Menu />
+        <Main />
+      </div>
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/component/searchbar/Searchbar.tsx
+++ src/component/searchbar/Searchbar.tsx
@@ -2,16 +2,10 @@ import React from "react";
 
 export default function Searchbar() {
   return (
-    <div className="field">
-      <p className="control has-icons-left has-icons-right">
-        <input className="input" type="email" placeholder="Email" />
-        <span className="icon is-small is-left">
-          <i className="fas fa-envelope"></i>
-        </span>
-        <span className="icon is-small is-right">
-          <i className="fas fa-check"></i>
-        </span>
-      </p>
-    </div>
+    <input
+      className="input is-primary is-half searchbar"
+      type="text"
+      placeholder="Primary input"
+    />
   );
 }
